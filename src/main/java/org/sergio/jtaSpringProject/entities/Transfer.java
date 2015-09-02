0102bb7a11ef44f95a86d53c8f666680a7edb8d8
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-02 09:25:07 +0200

    Create toString, equals and hashCode method in Transfer

--- src/main/java/org/sergio/jtaSpringProject/entities/Transfer.java
+++ src/main/java/org/sergio/jtaSpringProject/entities/Transfer.java
@@ -99,5 +99,63 @@ public class Transfer {
 		this.targetClient.sumAmount(this.amount);
 
 	}
+
+	@Override
+	public String toString() {
+		return "Transfer [id=" + id + ", date=" + date + ", concept=" + concept + ", amount=" + amount
+				+ ", originClient=" + originClient + ", targetClient=" + targetClient + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result + ((concept == null) ? 0 : concept.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((originClient == null) ? 0 : originClient.hashCode());
+		result = prime * result + ((targetClient == null) ? 0 : targetClient.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transfer other = (Transfer) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (concept == null) {
+			if (other.concept != null)
+				return false;
+		} else if (!concept.equals(other.concept))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (originClient == null) {
+			if (other.originClient != null)
+				return false;
+		} else if (!originClient.equals(other.originClient))
+			return false;
+		if (targetClient == null) {
+			if (other.targetClient != null)
+				return false;
+		} else if (!targetClient.equals(other.targetClient))
+			return false;
+		return true;
+	}
 	
 }
