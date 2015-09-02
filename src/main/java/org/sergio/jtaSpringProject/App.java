e99412a53915490e31d8b03e76112e648b78be82
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-02 09:11:12 +0200

    Refactor Class Main Test

--- src/main/java/org/sergio/jtaSpringProject/App.java
+++ src/main/java/org/sergio/jtaSpringProject/App.java
@@ -1,7 +1,9 @@
 package org.sergio.jtaSpringProject;
 
 import org.sergio.jtaSpringProject.entities.Animal;
+import org.sergio.jtaSpringProject.entities.Client;
 import org.sergio.jtaSpringProject.entities.Person;
+import org.sergio.jtaSpringProject.entities.Transfer;
 import org.sergio.jtaSpringProject.services.AppService;
 import org.sergio.jtaSpringProject.services.PersonService;
 import org.springframework.context.ApplicationContext;
@@ -15,49 +17,82 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Test Execution" );
         System.out.println("---------------");
-        //test1();
-        test2();
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        test1(context);
+        test2(context);
+        
+        System.out.println("---------------");
+        System.out.println( "End Test Execution" );
     }
 
     /*Acceso a servicios sin transacciones*/
-	private static void test1() {
-		//Obtenemos contexto de aplicacion utilizando Spring a traves del fichero de configuracion de Spring
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	private static void test1(ApplicationContext context) {
+		System.out.println("Begin Test 1");
 		
-		AppService personService = (AppService) context.getBean("appService");
+		AppService appService = (AppService) context.getBean("appService");
 		
-		Person person1 = new Person();
-		person1.setFirstName("Sergio");
-		person1.setLastName("Rodriguez");
+		Client originClient = new Client();
+		originClient.setName("John Doe");
+		originClient.setBalance(100.00);
 		
-		Animal animal1 = new Animal();
-		animal1.setName("Tiger");
+		Client targetClient = new Client();
+		targetClient.setName("Alice Doe");
+		targetClient.setBalance(1000.00);
 		
-		personService.createPersonAndAnimal(person1, animal1);
+		appService.createClient(originClient);
+		appService.createClient(targetClient);
 		
-		System.out.println("Test1 completado con exito");
+		Transfer transfer1 = new Transfer();
+		transfer1.setConcept("Flat water bill");
+		transfer1.setAmount(30.00);
+		transfer1.setOriginClient(originClient);
+		transfer1.setTargetClient(targetClient);
+		
+		try{
+			appService.makeTransfer(transfer1);
+			System.out.println("Test 1 Success");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("End Test 1");
 	}
 	
 	/*Acceso a servicios sin transacciones*/
-	private static void test2() {
-		//Obtenemos contexto de aplicacion utilizando Spring a traves del fichero de configuracion de Spring
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	private static void test2(ApplicationContext context) {
+		System.out.println("Begin Test 2");
 		
-		AppService personService = (AppService) context.getBean("appService");
+		AppService appService = (AppService) context.getBean("appService");
 		
-		Person person1 = new Person();
-		person1.setFirstName("Sergio");
-		person1.setLastName("Rodriguez");
+		Client originClient = new Client();
+		originClient.setName("John Doe");
+		originClient.setBalance(100.00);
 		
-		Animal animal1 = new Animal();
-		//Debe fallar por sobre pasar los caracteres del campo nombre, establecido en 255
-		animal1.setName("asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd");
+		Client targetClient = new Client();
+		targetClient.setName("Alice Doe");
+		targetClient.setBalance(1000.00);
 		
-		personService.createPersonAndAnimal(person1, animal1);
+		appService.createClient(originClient);
+		appService.createClient(targetClient);
 		
-		System.out.println("Test2 completado con exito");
+		Transfer transfer1 = new Transfer();
+		transfer1.setConcept("Flat water bill");
+		transfer1.setAmount(10000.00);
+		transfer1.setOriginClient(originClient);
+		transfer1.setTargetClient(targetClient);
+		
+		try{
+			appService.makeTransfer(transfer1);
+			System.out.println("Test 2 Success");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("End Test 2");
 	}
 	
 	
